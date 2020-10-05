0168705dceea17aef6a49a6542590124f5a84d3d
chadmorris55 <71134162+chadmorris55@users.noreply.github.com>
2020-10-05 09:48:52 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * finds if a number is a prime number or not
+ * @Chad 
  */
 public class Main {
 
@@ -9,7 +10,25 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // creates Scanner for user input
+    Scanner input = new Scanner(System.in);
     
-  }
+    // asks user for a number and records it for later
+    System.out.println("Which number would you like to check?");
+    int count = 2;
+    int number = input.nextInt();
+    int quotientCheck = 10;
+    while (count < number){
+      quotientCheck = number % count;
+      count = count + 1;
+      if (quotientCheck == 0){
+        break;
+      } 
+    } 
+    if (quotientCheck == 0){
+      System.out.println(number + " is not a prime number");
+    } else {
+      System.out.println(number + " is a prime number");
+    }
+  } 
 }
